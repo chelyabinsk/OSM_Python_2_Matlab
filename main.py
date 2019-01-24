e9a4f595dbc9814144c7f3a968f929f3b29b7e17
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2019-01-24 13:15:31 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+"""
+This code is pretty much just a copy of the example from 
+https://osmnx.readthedocs.io/en/stable/
+"""
+
+# -*- coding: utf-8 -*-
+
+#from keys import google_elevation_api_key #replace this with your own API key
+import osmnx as ox, networkx as nx, numpy as np
+import scipy as sp
+from scipy.io import savemat
+
+import csv
+
+lat = 50.9429
+long = 6.9619
+distance = 1000  # Distance (meters) from the starting point along the network 
+road_type = "bike"  # drive, drive_service, walk, bike, all, all_private
+
+G = ox.graph_from_point((lat,long), distance=distance, network_type=road_type)
+
+
+# Get position of each node
+keys = ["row_num"]  # # Get keys
+nodes = []  # Get nodes
+for node in G.nodes:
+    nodes.append(node)
+    for key in G.nodes.get(node).keys():
+        if(not key in keys):
+            keys.append(key)
+
+# Export positions data
+with open("node_pos.csv","w",newline='') as f:
+    wr = csv.writer(f)
+    wr.writerow(keys)
+    count = 1
+    for node in G.nodes:
+        row = {"row":count}
+        row.update(G.nodes.get(node))
+        w = csv.DictWriter(f,row.keys())
+        w.writerow(row)
+        count += 1
+
+
+# Get adjacency matrix from the graph
+A = nx.adjacency_matrix(G)
+# Add weights
+w,h = A.get_shape()
+for i in range(w):
+    for j in range(h):
+        if(A[i,j] == 1):
+            A[i,j] = G.get_edge_data(nodes[i],nodes[j])[0]["length"]
+
+#sp.sparse.save_npz('sparse_matrix.npz', A)
+savemat('temp', {'M':A})
+
